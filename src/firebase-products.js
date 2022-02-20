4e3516800045f5bb88df872fe72c03fb82047856
RobertoZeppilli <robzep171@gmail.com>
2022-02-20 19:05:04 +0100

    products.js file added and filled, next branch will upload all to DB

---
+++ src/firebase-products.js
@@ -0,0 +1,72 @@
+export const products = [
+    {
+        name: "nu3 Peanut Butter",
+        description: "Peanut Butter (Burro di arachidi) - 1 kg - crema di arachidi 100% naturale - Puro burro di arachidi proteico senza zuccheri aggiunti - Produzione controllata.",
+        category: "food",
+        image: "https://m.media-amazon.com/images/I/71fD15hM2gS._AC_SL1500_.jpg",
+        price: 15.99
+    },
+    {
+        name: "Xiaomi Redmi Note 10 Pro",
+        description: "Xiaomi Redmi Note 10 Pro - Smartphone 128GB, 6GB RAM, Dual Sim, Onyx Gray ",
+        category: "electronics",
+        image: "https://m.media-amazon.com/images/I/51UyTHwP6wL._AC_SL1000_.jpg",
+        price: 269.00
+    },
+    {
+        name: "Lenovo IdeaPad 3 Chromebook",
+        description: "Lenovo IdeaPad 3 Chromebook - Display 15.6 - (FullHD Processore Intel Celeron N4500, 64 GB eMMC, RAM 8 GB, WiFi 6 Chrome OS) - Arctic Grey ",
+        category: "electronics",
+        image: "https://m.media-amazon.com/images/I/81PzfsKkDxL._AC_SL1500_.jpg",
+        price: 286.00
+    },
+    {
+        name: "JBL Tune500BT",
+        description: "JBL Tune500BT Cuffie Wireless Sovraurali con funzione Multipoint e Ricarica veloce – Cuffie On-Ear Bluetooth con connessione a Siri e Google – Fino a 16h di autonomia, Nero",
+        category: "electronics",
+        image: "https://m.media-amazon.com/images/I/51WKFjmQSKL._AC_SL1196_.jpg",
+        price: 28.90
+    },
+    {
+        name: "Pringles",
+        description: "Pringles Chips di Patate Sour Cream e Onion, 175g",
+        category: "food",
+        image: "https://m.media-amazon.com/images/I/6111n-gh51L._AC_SL1500_.jpg",
+        price: 3.45
+    },
+    {
+        name: "Legami",
+        description: "Legami - Vulcano Pulisci Microonde, 14X12 cm, Pulisce a Fondo il Microonde in 5 Minuti a Massimo 500 W di Potenza, in Plastica, da Utilizzare con Aceto e Acqua",
+        category: "home",
+        image: "https://m.media-amazon.com/images/I/61GgALgacUL._AC_SL1500_.jpg",
+        price: 10.59
+    },
+    {
+        name: "Proscenic 850T",
+        description: "Proscenic 850T Robot Aspirapolvere Lavapavimenti, Aspirazione Potenza 3000Pa Controllo APP & Alexa, Serbatoio Acqua Elettrico 2-In-1 per Pulizia...",
+        category: "home",
+        image: "https://m.media-amazon.com/images/I/61E8A5DmuML._AC_SL1500_.jpg",
+        price: 289.90
+    },
+    {
+        name: "Ragù alla Bolognese",
+        description: "Barilla Sugo Ragù alla Bolognese, Salsa Pronta al Pomodoro Italiano e Carne Selezionata senza Glutine, 400g",
+        category: "food",
+        image: "https://m.media-amazon.com/images/I/71pMfWpJIlL._AC_SL1500_.jpg",
+        price: 1.70
+    },
+    {
+        name: "Tablet Fire 7",
+        description: "Tablet Fire 7, schermo da 7, 16 GB, (Nero) - Con pubblicità",
+        category: "electronics",
+        image: "https://m.media-amazon.com/images/I/51y1qd82dzL._AC_SL1000_.jpg",
+        price: 69.99
+    },
+    {
+        name: "Sedia da Ufficio",
+        description: "Sedia Girevole NET 90 Tessuto in Maglia / Tessuto Nero / Blu",
+        category: "home",
+        image: "https://m.media-amazon.com/images/I/81BCKiBISBL._AC_SL1500_.jpg",
+        price: 289.90
+    },
+]
