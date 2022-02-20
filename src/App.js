b64699f87c8be70b1398703d84b4a43b6784219d
RobertoZeppilli <robzep171@gmail.com>
2022-02-20 15:29:09 +0100

    routes and paths added in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import { HomePage } from './pages/pages-provider/pages-provider'
+// pages and paths
+import { HomePage, Login, Register, ProductInfo, Cart } from './pages/pages-provider/pages-provider'
+import PATHS from './pages/pages-provider/paths'
 
+// routes
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-danger">FIRECOMMERCE</h1>
-      <HomePage />
+      <Router>
+        <Routes>
+          <Route exact="true" path={PATHS.HOME} element={<HomePage />} />
+          <Route exact="true" path={PATHS.LOGIN} element={<Login />} />
+          <Route exact="true" path={PATHS.REGISTER} element={<Register />} />
+          <Route exact="true" path={PATHS.PRODUCT_INFO} element={<ProductInfo />} />
+          <Route exact="true" path={PATHS.CART} element={<Cart />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/pages-provider/paths.js
@@ -0,0 +1,9 @@
+const PATHS = {
+    HOME: "/",
+    LOGIN: "/login",
+    REGISTER: "/register",
+    CART: "/cart",
+    PRODUCT_INFO: "/productinfo"
+}
+
+export default PATHS
