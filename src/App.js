eb120f4923432f944e652ae4312e7cd54f228507
RobertoZeppilli <robzep171@gmail.com>
2022-02-20 15:17:38 +0100

    scaffholding and creating first pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { HomePage } from './pages/pages-provider/pages-provider'
+
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-danger">FIRECOMMERCE</h1>
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/pages-provider/pages-provider.js
@@ -0,0 +1,5 @@
+export { default as HomePage } from '../HomePage'
+export { default as Login } from '../Login'
+export { default as Cart } from '../Cart'
+export { default as Register } from '../Register'
+export { default as ProductInfo } from '../ProductInfo'
